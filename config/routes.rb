aff0caa645cb72ecee82a52c2ae6499811e4a9cc
Serhii Yarovyi <cant1987m@gmail.com>
2021-01-13 12:15:46 +0200

    Added product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
   root "products#index"
+  # resources :categories do
+  #   resources :products
+  # end
+  resources :categories
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
